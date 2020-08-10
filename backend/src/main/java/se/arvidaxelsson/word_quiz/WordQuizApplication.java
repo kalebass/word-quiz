8efd55e765032bc8c69276db27e74f3c32a0d273
Arvid <mail@arvidaxelsson.se>
2020-08-11 01:03:34 +0200

    Initial commit

---
+++ backend/src/main/java/se/arvidaxelsson/word_quiz/WordQuizApplication.java
@@ -0,0 +1,11 @@
+package se.arvidaxelsson.word_quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordQuizApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WordQuizApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/se/arvidaxelsson/word_quiz/WordQuizApplicationTests.java
@@ -0,0 +1,11 @@
+package se.arvidaxelsson.word_quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordQuizApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
