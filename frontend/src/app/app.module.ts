6592b8a809150054643955a82bf3c242ba6f1632
Arvid <mail@arvidaxelsson.se>
2020-10-17 07:27:02 +0200

    Show options in table

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { QuizComponent } from './quiz/quiz.component';
         BrowserAnimationsModule,
         BrowserModule,
         HttpClientModule,
+        MatTableModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
--- frontend/src/app/quiz/quiz.component.ts
+++ frontend/src/app/quiz/quiz.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { SelectionModel } from '@angular/cdk/collections';
+import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { sampleSize, shuffle } from 'lodash-es';
 import { Word } from '../word';
 import { WordService } from '../word.service';
+
+const numChoices = 7;
 
 @Component({
     selector: 'app-quiz',
     templateUrl: './quiz.component.html',
     styleUrls: ['./quiz.component.css'],
 })
-export class QuizComponent implements OnInit {
-    words: Word[] = [];
+export class QuizComponent {
+    answers: Word[] = [];
+    selection = new SelectionModel<Word>(false);
+    dataSource = new MatTableDataSource<Word>();
+    displayedColumns = ['zh', 'pinyin', 'en'];
 
     constructor(wordService: WordService) {
-        wordService.getWords('Lesson 1').subscribe(words => this.words = words);
+        wordService.getWords('Lesson 1').subscribe(words => {
+            this.answers = shuffle(words);
+            this.nextQuestion();
+        });
     }
 
-    ngOnInit(): void {
+    nextQuestion(): void {
+        this.dataSource.data = sampleSize(this.answers, numChoices);
     }
 }
