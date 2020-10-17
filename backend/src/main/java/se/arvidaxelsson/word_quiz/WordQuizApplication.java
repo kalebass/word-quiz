5a5564352530cc4163ac57ef5c25d3637dd9ee6d
Arvid <mail@arvidaxelsson.se>
2020-10-17 12:29:20 +0200

    Fix incorrect mapping of Word record components

--- backend/src/main/java/se/arvidaxelsson/word_quiz/WordQuizApplication.java
+++ backend/src/main/java/se/arvidaxelsson/word_quiz/WordQuizApplication.java
@@ -1,11 +1,18 @@
 package se.arvidaxelsson.word_quiz;
 
+import org.jooq.conf.Settings;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WordQuizApplication {
     public static void main(String[] args) {
         SpringApplication.run(WordQuizApplication.class, args);
+    }
+
+    @Bean
+    public Settings jooqSettings() {
+        return new Settings().withMapConstructorParameterNames(true);
     }
 }
