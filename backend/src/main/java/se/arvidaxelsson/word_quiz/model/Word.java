a6fbbb3fcdfa6be9816c933f6ed607caa9eb9a71
Arvid Axelsson <mail@arvidaxelsson.se>
2021-05-22 21:29:51 +0200

    Remove unnecessary JsonProperty annotation

--- backend/src/main/java/se/arvidaxelsson/word_quiz/model/Word.java
+++ backend/src/main/java/se/arvidaxelsson/word_quiz/model/Word.java
@@ -1,10 +1,7 @@
 package se.arvidaxelsson.word_quiz.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public record Word(
-        @JsonProperty String zh,
-        @JsonProperty String pinyin,
-        @JsonProperty String en
-) {
+        String zh,
+        String pinyin,
+        String en) {
 }
